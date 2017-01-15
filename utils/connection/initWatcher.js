c668fb2fb7ce950403463bea6f832c165f457e68
Nicholas Carter <nicholas.b.carter@gmail.com>
2017-01-15 15:19:12 -0500

    adding utils

---
+++ utils/connection/initWatcher.js
@@ -0,0 +1,16 @@
+const ngrok         = require('ngrok');
+const open          = require('open');
+const dotenv        = require('dotenv');
+
+export.default = () => {
+  //load environment variables,
+  //either from .env files (development),
+  //heroku environment in production, etc...
+  dotenv.load();
+
+  if (process.env.NODE_ENV !== 'production') {
+    // kick off the ngrok tunnel if were not in prod and open the browser...
+    open('http://localhost:4040');
+    ngrok.connect(function (err, url) {});
+  }
+};
